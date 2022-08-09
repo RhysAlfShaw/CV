20ea1f7ab10f99347d8484d4b0d877c5d5548397
Rhys Shaw <45737158+RhysAlfShaw@users.noreply.github.com>
2022-08-09 14:43:39 +0100

    Update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -25,14 +25,14 @@ module.exports = {
   
   experience: [
     {
-      header: 'Performance',
+      header: 'MSc Project: Studing the effect of Substucture in Star Clusters.',
       skills: ['theater', 'play'],
       contents: `
 Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
 `
     },
     {
-      header: 'Poetry',
+      header: 'BSc Project: Examining Galaxy Cluster Properties from seperatly selected samples.',
       skills: ['poetry', 'erotic'],
       contents: `
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
